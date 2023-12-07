f13ab4db71e9fdd8cceb9beb33c79ff6ee3a0d60
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-12-07 16:53:15 +0200

    Update ProductDetalis.jsx

--- src/components/pages/ProductDetalis/ProductDetalis.jsx
+++ src/components/pages/ProductDetalis/ProductDetalis.jsx
@@ -2,15 +2,12 @@ import { useSearchParams } from 'react-router-dom';
 
 export const ProductDetalis = () => {
   const { id } = useSearchParams();
-  const product = getProductById(id);
 
   return (
     <div>
       <img src="https://via.placeholder.com/960x240" alt="" />
 
-      <h2>
-        Product - {product.name} - {id}
-      </h2>
+      <h2>Product - Name - {id}</h2>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus sunt
         excepturi nesciunt iusto dignissimos assumenda ab quae cupiditate a, sed
