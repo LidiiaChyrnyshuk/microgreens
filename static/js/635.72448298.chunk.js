816468f31aef906a5ec621d8d1dce72cdb5f58c1
LidiiaChyrnyshuk <114225851+LidiiaChyrnyshuk@users.noreply.github.com>
2023-12-27 14:22:58 +0000

    Deploying to gh-pages from @ LidiiaChyrnyshuk/microgreens@2494545d9ae06e1999254b8ea5dc14fa9d992bea 🚀

--- static/js/635.72448298.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmicrogreens=self.webpackChunkmicrogreens||[]).push([[635],{635:function(e,n,r){r.r(n),r.d(n,{default:function(){return s}});var c=r(184);function s(){return(0,c.jsx)("section",{children:(0,c.jsx)("h2",{children:"Products"})})}}}]);
-//# sourceMappingURL=635.72448298.chunk.js.map
